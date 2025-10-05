54b0391c0e0faca437fcd0ef9dd29fff466f546d
Dawid Pietras <dawid.pietras.91@gmail.com>
2025-10-05 23:22:00 +0200

    service tested, waiting for cron confirmation

--- lego_scrapper/app.py
+++ lego_scrapper/app.py
@@ -2,57 +2,96 @@ import requests
 import json
 import yaml
 import os
+import re
+import logging
+import sys
 
 from bs4 import BeautifulSoup as bs
+from pydantic import BaseModel
+from typing import Optional
 
 URL = "https://www.lego.com/pl-pl/product/captain-jack-sparrows-pirate-ship-10365"
 SYSTEM_CA_BUNDLE = '/etc/ssl/certs/ca-certificates.crt'
 
-APP_TOKEN = os.environ.get("LEGO_APP_TOKEN", 'axqb5973zrcu3ed7bmya8e13izhx6z')
-USER_TOKEN = os.environ.get("LEGO_USER_TOKEN", 'u65vsygubbicdo7uskjj477zvh1ah9')
-PUSHOVER_URL = os.environ.get("PUSHOVER_URL", "https://api.pushover.net/1/messages.json")
+APP_TOKEN = os.environ.get("LEGO_APP_TOKEN")
+USER_TOKEN = os.environ.get("LEGO_USER_TOKEN")
+PUSHOVER_URL = os.environ.get("PUSHOVER_URL")
 
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36'}
 
+REGEX = r"\b(\d{1,2})\s+(stycznia|lutego|marca|kwietnia|maja|czerwca|lipca|sierpnia|września|października|listopada|grudnia)\s+(\d{4})\b"
 
-def get_data_from_web():
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    stream=sys.stderr
+)
+
+logger = logging.getLogger(__name__)
+
+class LegoData(BaseModel):
+    price: Optional[str] = "N/A"
+    availability: Optional[str] = "N/A"
+    
+    def format_pushover_message(self) -> str:
+        return f'Cena: {self.price}, Wysyłka: {self.availability}'
+
+
+def get_data_from_web() -> LegoData:
     
     """
-        Return data from LEGO page regarding price and availability.
+    Return data from LEGO page regarding price and availability.
+    
     """
     
-    response = requests.get(URL, headers=headers, verify=SYSTEM_CA_BUNDLE)
-    response.raise_for_status()
+    data_dict = {
+        'price': 'N/A',
+        'availability': 'N/A'
+    }
+    
+    logger.info("Starting data retriving from LEGO website.")
+    
+    try:
+        response = requests.get(URL, headers=HEADERS, verify=SYSTEM_CA_BUNDLE)
+        response.raise_for_status()
+        logger.info("Succesfully scraped the website. Status 200.")
+    except requests.exceptions.RequestException as e:
+        logger.error("Connection error: {e}", exc_info=True)
+        raise e
 
     soup = bs(response.text, 'html.parser')
 
     price_element = soup.select_one('span[data-test="product-price-display-price"]')
     if price_element:
-        price = price_element.get_text(strip=True)
-        print(f"Cena: {price}")
-    else:
-        print("Cena: Element nie został znaleziony w statycznym HTML.")
+        data_dict['price'] = price_element.get_text(strip=True)
+        logger.info("Price captured!")
         
     availability_element = soup.select_one('span[data-test="product-overview-availability"]')
+    regex_availability = availability_element.get_text(strip=True)
+    match = re.search(REGEX, regex_availability).group()
+    if match:
+        data_dict['availability'] = match
+        logger.info("Availability captured!")
+    
+    return LegoData(**data_dict)
 
-    if availability_element:
-        availability = availability_element.get_text(strip=True).split('się')[1]
-        print(f"Dostępność: {availability}")
-    else:
-        print("Dostępność: Element nie został znaleziony w statycznym HTML.")
+def pushover_handler(lego_data: LegoData):
 
-def pushover_handler()
+    payload = {
+        "token": APP_TOKEN,
+        'user': USER_TOKEN,
+        "message": lego_data.format_pushover_message(),
+        "title": 'Lego Jack\'s Ship Status',
+        "sound": "bike"
+    }
 
-payload = {
-    "token": APP_TOKEN,
-    'user': USER_TOKEN,
-    "message": None,
-    "title": 'Lego Jack\'s Ship Status',
-    "sound": "bike"
-}
+    try:
+        response = requests.post(PUSHOVER_URL, data = payload, timeout=5)
+        response.raise_for_status()
+        logger.info("Message successfully pushed.")
+    except requests.exceptions.RequestException as e:
+        logger.error("Connection error: {e}", exc_info=True)
 
-payload['message'] = f'{price} {availability}'
-
-response = requests.post(PUSHOVER_URL, data = payload)
-
-print(response.status_code)
+if __name__ == "__main__":
+    data = get_data_from_web()
+    pushover_handler(data)
